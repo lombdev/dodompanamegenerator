2a348b5e81f05456eabe71de5013f65839089c9f
LombDev <ruihorta88@hotmail.com>
2016-06-09 23:56:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var arrAnswer = new Array(15);
 
-	arrAnswer[0] = "Devil Sapato";
-	arrAnswer[1] = "Evil Vassoura";
-	arrAnswer[2] = "Dark Otário";
+	arrAnswer[0] = "Devil {0}";
+	arrAnswer[1] = "Evil {0}";
+	arrAnswer[2] = "Dark {0}";
 
 function AskQuestion()
 {
