5fa6591f9a81965dd721506a0882bcc52d614507
LombDev <ruihorta88@hotmail.com>
2016-06-10 09:53:41 +0100

    Create effects.js

---
+++ effects.js
@@ -0,0 +1,25 @@
+function blink(ob)
+{
+if (ob.style.visibility == "visible" )
+{
+ob.style.visibility = "hidden";
+}
+else
+{
+ob.style.visibility = "visible";
+}
+}
+setInterval("blink(bl)",10);
+
+function blink(ob)
+{
+if (ob.style.visibility == "visible" )
+{
+ob.style.visibility = "hidden";
+}
+else
+{
+ob.style.visibility = "visible";
+}
+}
+setInterval("blink(bl2)",500);
