dd648cbe09740ac499bc58336fc604f752dd6826
LombDev <ruihorta88@hotmail.com>
2016-06-10 09:51:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,29 @@ function AskQuestion(){
 function resetAnswer(){
 	lblAnswer.innerText = "...";
 }
+
+function blink(ob)
+{
+if (ob.style.visibility == "visible" )
+{
+ob.style.visibility = "hidden";
+}
+else
+{
+ob.style.visibility = "visible";
+}
+}
+setInterval("blink(bl)",10);
+
+function blink(ob)
+{
+if (ob.style.visibility == "visible" )
+{
+ob.style.visibility = "hidden";
+}
+else
+{
+ob.style.visibility = "visible";
+}
+}
+setInterval("blink(bl2)",500);
